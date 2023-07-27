f7635a15738d073be3b33abe78ad5643c8bc1db0
gameoflord012 <manhhieu012@gmail.com>
2023-07-27 20:16:52 +0700

    restruct

---
+++ Donut/donut.cpp
@@ -0,0 +1,114 @@
+#include "donut.h"
+#include <SDL2/SDL_image.h>
+
+SDL_Window* Donut::gWindow = NULL;
+SDL_Renderer* Donut::gRenderer = NULL;
+
+
+bool Donut::init(int screenWidth, int screenHeight)
+{
+    //Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		{
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+
+		//Create window
+		Donut::gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN );
+		if( Donut::gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			//Create renderer for window
+			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+
+			//try software render if hardware fails
+			if( gRenderer == NULL )
+			{
+				SDL_Log( "Accelerated renderer could not be created! SDL Error: %s\nSwitching to software renderer", SDL_GetError() );
+				gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_SOFTWARE);
+			}
+
+			if( gRenderer == NULL )
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+void Donut::rendererCopySprite( Sprite sprite )
+{
+	SDL_RenderCopy( Donut::gRenderer, sprite.texture, NULL, &sprite.rect );
+}
+
+SDL_Texture* Donut::loadTexture( const char * path )
+{
+	//The final texture
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load( path );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
+	}
+	else
+	{
+		//Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+		if( newTexture == NULL )
+		{
+			printf( "Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError() );
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	return newTexture;
+}
+
+void Donut::clean()
+{
+    //Destroy window	
+    SDL_DestroyRenderer( gRenderer );
+    SDL_DestroyWindow( gWindow );
+    
+    gWindow = NULL;
+    gRenderer = NULL;
+
+    //Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
+}
--- main.cpp
+++ main.cpp
@@ -9,57 +9,8 @@ and may not be redistributed without written permission.*/
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-#include "donut.h"
-#include "utils.h"
-
-void keyDown(int keycode);
-
-bool quit = false;
-Sprite donut;
-
-void loop_handler(void*)
-{
-	//Event handler
-	SDL_Event e;
-	//Handle events on queue
-	while( SDL_PollEvent( &e ) != 0 )
-	{
-		//User requests quit
-		if( e.type == SDL_QUIT )
-		{
-			quit = true;
-		}
-		else if( e.type == SDL_KEYDOWN )
-		{
-			keyDown(e.key.keysym.sym);
-		}
-	}
-
-	//Clear screen
-	SDL_RenderClear( Donut::gRenderer );
-
-	//Render texture to screen
-	Donut::rendererCopySprite(donut);
-
-	//Update screen
-	SDL_RenderPresent( Donut::gRenderer );
-
-}
-
-void keyDown(int keycode)
-{
-	switch (keycode)
-	{
-	case SDLK_LEFT:
-		donut.rect.x -= 5;
-		break;
-	case SDLK_RIGHT:
-		donut.rect.x += 5;
-		break;
-	default:
-		break;
-	}
-}
+#include "Donut/donut.h"
+#include "Donut/utils.h"
 
 int main( int argc, char* args[] )
 {
@@ -67,20 +18,61 @@ int main( int argc, char* args[] )
 	if( !Donut::init(1000, 500) )
 	{
 		printf( "Failed to initialize!\n" );
+		return -1;
 	}
-	else
-	{
-		//While application is running
-		donut.rect.x = 0;
-		donut.rect.y = 0;
-		donut.rect.w = 100;
-		donut.rect.h = 200;
-		donut.texture = Donut::loadTexture(DONUT_ASSETS_DIR(donut.png));
 
-		while( !quit )
+	Sprite donut;
+
+	donut.rect.x = 0;
+	donut.rect.y = 0;
+	donut.rect.w = 100;
+	donut.rect.h = 200;
+	donut.texture = Donut::loadTexture(DONUT_ASSETS_DIR(donut.png));
+
+	SDL_Event e;
+	bool quit = false;
+
+	Uint32 startTime = SDL_GetTicks();
+	float deltaTimeInSeconds;
+
+	while(!quit)
+	{
+		deltaTimeInSeconds = (float)(SDL_GetTicks() - startTime) / 1000.;
+		startTime = SDL_GetTicks();
+
+		while( SDL_PollEvent( &e ) != 0 )
 		{
-			loop_handler(NULL);	
+			//User requests quit
+			if( e.type == SDL_QUIT )
+			{
+				quit = true;
+			}
+			else if( e.type == SDL_KEYDOWN )
+			{
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_LEFT:
+					donut.rect.x -= 100 * deltaTimeInSeconds;
+					break;
+				case SDLK_RIGHT:
+					donut.rect.x += 100 * deltaTimeInSeconds;
+					break;
+				default:
+					break;
+				}
+			}
 		}
+
+		printf("%f\n", deltaTimeInSeconds);
+		
+		//Clear screen
+		SDL_RenderClear( Donut::gRenderer );
+
+		//Render texture to screen
+		Donut::rendererCopySprite(donut);
+
+		//Update screen
+		SDL_RenderPresent( Donut::gRenderer );
 	}
 
 	//Free resources and close SDL
