a037a1b7aa0590b4a926cb2612934986c41200a6
gameoflord012 <manhhieu012@gmail.com>
2023-08-01 08:35:59 +0700

    .

---
+++ Donut/Donut_GL_DrawLineProgram.cpp
@@ -0,0 +1,39 @@
+#include "Donut_GL_DrawLineProgram.h"
+
+void Donut_GL_DrawLineProgram::useProgram()
+{
+	glUseProgram(mProgramId);
+}
+
+Donut_GL_DrawLineProgram::Donut_GL_DrawLineProgram(/* args */)
+{
+    mProgramId = glCreateProgram();
+
+    Donut_ShaderSource vertexShaderSource(GL_VERTEX_SHADER);
+	vertexShaderSource.compileShader(Donut_readfile(Donut_GetShadersPath("vertexShader.txt")).c_str());
+
+	Donut_ShaderSource fragmentShaderSource(GL_FRAGMENT_SHADER);
+	fragmentShaderSource.compileShader(Donut_readfile(Donut_GetShadersPath("fragmentShader.txt")).c_str());
+
+    glAttachShader(mProgramId, fragmentShaderSource.getShader());
+	glAttachShader(mProgramId, vertexShaderSource.getShader());
+	glLinkProgram(mProgramId);
+
+	GLint  success;
+	char infoLog[512];
+	glGetProgramiv(mProgramId, GL_LINK_STATUS, &success);
+
+	if(success != GL_TRUE) {
+    	glGetProgramInfoLog(mProgramId, 512, NULL, infoLog);
+		Donut_LogError("Shader program failed: %s", infoLog);
+
+		Donut_Assert(false);
+	}
+
+    Donut_glCheckErrorAll();
+}
+
+Donut_GL_DrawLineProgram::~Donut_GL_DrawLineProgram()
+{
+	glDeleteProgram(mProgramId);
+}
--- Donut/Donut_GL_DrawLineProgram.h
+++ Donut/Donut_GL_DrawLineProgram.h
@@ -16,44 +16,6 @@ public:
     ~Donut_GL_DrawLineProgram();
 };
 
-void Donut_GL_DrawLineProgram::useProgram()
-{
-	glUseProgram(mProgramId);
-}
-
-Donut_GL_DrawLineProgram::Donut_GL_DrawLineProgram(/* args */)
-{
-    mProgramId = glCreateProgram();
-
-    Donut_ShaderSource vertexShaderSource(GL_VERTEX_SHADER);
-	vertexShaderSource.compileShader(Donut_readfile(Donut_GetShadersPath("vertexShader.txt")).c_str());
-
-	Donut_ShaderSource fragmentShaderSource(GL_FRAGMENT_SHADER);
-	fragmentShaderSource.compileShader(Donut_readfile(Donut_GetShadersPath("fragmentShader.txt")).c_str());
-
-    glAttachShader(mProgramId, fragmentShaderSource.getShader());
-	glAttachShader(mProgramId, vertexShaderSource.getShader());
-	glLinkProgram(mProgramId);
-
-	GLint  success;
-	char infoLog[512];
-	glGetProgramiv(mProgramId, GL_LINK_STATUS, &success);
-
-	if(success != GL_TRUE) {
-    	glGetProgramInfoLog(mProgramId, 512, NULL, infoLog);
-		Donut_LogError("Shader program failed: %s", infoLog);
-
-		Donut_Assert(false);
-	}
-
-    Donut_glCheckErrorAll();
-}
-
-Donut_GL_DrawLineProgram::~Donut_GL_DrawLineProgram()
-{
-	glDeleteProgram(mProgramId);
-}
-
 #endif
 
 	// Donut_glCall(glGenVertexArrays(1, &VAO));
